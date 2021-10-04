86c873579c08bacd56e747da27e1fb0aaa7751f3
ArchLeaders <archparliament@gmail.com>
2021-10-03 23:53:33 -0700

    Purged unecassary files.

---
+++ BotW Installer/Libraries/Files/Write.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace BotW_Installer.Libraries.Settings
+{
+    class Write
+    {
+        public static async Task Xml(string outputXml, string botw_entry_point, string titleId, string region, string path_to_uking)
+        {
+            await Task.Run(() => File.WriteAllText(Files.Settings.Xml(botw_entry_point, titleId, region, path_to_uking), outputXml));
+        }
+
+        public static async Task Json(string outputJson, string cemuDir, string baseGame, string update, string storeDir, string dlc = null)
+        {
+            await Task.Run(() => File.WriteAllText(Files.Settings.Json(cemuDir, baseGame, update, storeDir), outputJson));
+        }
+    }
+}
---
+++ Setup/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Setup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Embed("installer.resource", "Installer.exe");
+            Embed("7z.resource", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\.botw\\7z.resource");
+            Embed("installer.resource", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\.botw\\Installer.exe");
+        }
+
+        static void Embed(string fileName, string output)
+        {
+            Assembly assembly = Assembly.GetCallingAssembly();
+
+            using (Stream stream = assembly.GetManifestResourceStream("Setup.Resource." + fileName))
+            using (BinaryReader binaryReader = new BinaryReader(stream))
+            using (FileStream fileStream = new FileStream(output, FileMode.OpenOrCreate))
+            using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
+                binaryWriter.Write(binaryReader.ReadBytes((int)stream.Length));
+        }
+    }
+}
