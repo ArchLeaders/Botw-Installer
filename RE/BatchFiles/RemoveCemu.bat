219dd4f0800ed0a015131d558c502cf7fbb826ff
Marcus S <80713508+ArchLeaders@users.noreply.github.com>
2022-02-25 01:32:45 -0800

    Cemu uninstaller (RE Temp)

---
+++ RE/BatchFiles/RemoveCemu.bat
@@ -0,0 +1,26 @@
+﻿@ECHO off
+
+:: Delete Cemu Directory
+ECHO Removing Cemu . . .
+FOR %%F IN ("$cemu") DO (
+	DEL "%%F" /Q /F
+)
+FOR /D %%D IN "$cemu" DO (
+	:: Skip mlc01 folder
+	IF NOT "%%D"=="$cemu\mlc01" (
+		RMDIR "%%D" /Q
+	)
+)
+
+:: Remove Shortcuts
+ECHO Removing shortcuts . . .
+DEL "$desktop\Cemu.lnk" /Q /F
+DEL "$start\Cemu.lnk" /Q /F
+
+:: Remove Registry Key
+ECHO Removing registry keys . . .
+REG delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Cemu" /F
+
+:: Delete Self
+PAUSE
+DEL "$root\cemu.bat" /Q /F
