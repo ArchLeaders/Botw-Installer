9e254ea824e9d415226474dc7cdf684dc460a3d4
Marcus Smith <80713508+ArchLeaders@users.noreply.github.com>
2022-03-04 01:14:52 -0800

    Add mirror python module file

---
+++ RE/python_module.py
@@ -0,0 +1,9 @@
+﻿import sys
+from bcml import install
+from pathlib import Path
+
+def main():
+    install.install_mod(Path(sys.argv[1]), merge_now=bool(sys.argv[2]=='true'))
+
+if __name__ == '__main__':
+    main()
