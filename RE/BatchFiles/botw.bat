8ba11e69a70047e4c243c85b1aec36da8b615162
Marcus Smith <80713508+ArchLeaders@users.noreply.github.com>
2022-03-05 11:34:34 -0800

    Update botw.bat

--- RE/BatchFiles/botw.bat
+++ RE/BatchFiles/botw.bat
@@ -1,7 +1,7 @@
-﻿@ECHO OFF
+@ECHO OFF
 
 :: Start BotW
-START "BOTW" "$cemu\Cemu.exe" -g -f "$base\code\U-King.rpx"
+START "BOTW" "$cemu\Cemu.exe" --game "$base\code\U-King.rpx" -f
 
 :: Start DS4Windows if it exists
 IF EXIST "$ds4\DS4Windows.exe" (
