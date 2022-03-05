1bf3a102bb80816d4db5801222381feefedc35b1
Marcus Smith <80713508+ArchLeaders@users.noreply.github.com>
2022-03-05 14:53:18 -0800

    Mirror BCML uninstaller

---
+++ RE/BatchFiles/bcml.bat
@@ -0,0 +1,21 @@
+﻿@ECHO OFF
+
+:: Remove BCML Data
+ECHO Removing BCML . . .
+RMDIR "C:\Users\ArchLeaders\AppData\Local\bcml" /S /Q
+
+:: Remove BCML Package
+START "PIP" "$python\Scripts\pip.exe" uninstall -y bcml
+
+:: Remove Shortcuts
+ECHO Removing shortcuts . . .
+DEL "$desktop\BCML.lnk" /Q /F
+DEL "$start\BCML.lnk" /Q /F
+
+:: Remove Registry Key
+ECHO Removing registry keys . . .
+REG delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\BCML" /F
+
+:: Delete Self
+PAUSE
+DEL "$root\bcml.bat" /Q /F
