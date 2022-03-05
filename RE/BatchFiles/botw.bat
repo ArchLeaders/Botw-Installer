bff99ee5ce966ee9edbc287b09024cf54da21596
Marcus Smith <80713508+ArchLeaders@users.noreply.github.com>
2022-03-04 21:28:20 -0800

    Add files via upload

---
+++ RE/BatchFiles/botw.bat
@@ -0,0 +1,9 @@
+﻿@ECHO OFF
+
+:: Start BotW
+START "BOTW" "$cemu\Cemu.exe" -g -f "$base\code\U-King.rpx"
+
+:: Start DS4Windows if it exists
+IF EXIST "$ds4\DS4Windows.exe" (
+    START "DS4" "$ds4\DS4Windows.exe"
+)
