22d9e561049dde2412ed659610458efa2ed423d6
Marcus Smith <80713508+ArchLeaders@users.noreply.github.com>
2022-03-06 18:58:19 -0800

    No longer necessary

--- RE/BatchFiles/botw.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-:: Start BotW
-START "BOTW" "$cemu\Cemu.exe" --game "$base\code\U-King.rpx" -f
-
-:: Start DS4Windows if it exists
-IF EXIST "$ds4\DS4Windows.exe" (
-    START "DS4" "$ds4\DS4Windows.exe"
-)
--- RE/python_module.py
+++
@@ -1,9 +0,0 @@
-﻿import sys
-from bcml import install
-from pathlib import Path
-
-def main():
-    install.install_mod(Path(sys.argv[1]), merge_now=bool(sys.argv[2]=='true'))
-
-if __name__ == '__main__':
-    main()
